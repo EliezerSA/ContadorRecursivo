cdf5468a5ef53abe0bf00c392961f5137434eafb
EliezerSA <eliezersantiago-100@outlook.com>
2022-03-15 15:15:48 -0600

    Agrega el Proyecto

---
+++ src/ContadorRecursivo.java
@@ -0,0 +1,13 @@
+
+
+public class ContadorRecursivo {
+	public void imprimir(int a) {
+		if(a <=1000) {
+			System.out.print(a + " ");
+			imprimir(a + 1);
+			
+		}
+		
+	}//1
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		ContadorRecursivo recursividad = new ContadorRecursivo();
+		recursividad.imprimir(1);
+	}
+	
+}
